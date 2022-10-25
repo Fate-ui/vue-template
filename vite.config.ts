02afd07d6c8f8b4ed167109b5ea1cee3702b3bf0
ween <228574227@qq.com>
2022-10-25 14:05:34 +0800

    add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import { createVitePlugins } from './config/vitePlugin'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
   plugins: createVitePlugins()
 })
