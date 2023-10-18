33aa8d13b0dcabb607ebf1e3baf9aca66239ef18
ween <228574227@qq.com>
2023-10-18 11:56:26 +0800

    添加插件

--- config/vitePlugin/index.ts
+++ config/vitePlugin/index.ts
@@ -1,6 +1,9 @@
 import vue from '@vitejs/plugin-vue'
 import { visualizer } from 'rollup-plugin-visualizer'
 import { unocssPlugin } from './unocss'
+import { ConfigRestartPlugin } from './restart'
+import { AutoRegistryComponents } from './component'
+import { AutoImportDeps } from './autoImport'
 import type { PluginOption } from 'vite'
 
 export function createVitePlugins(): PluginOption[] {
@@ -9,6 +12,12 @@ export function createVitePlugins(): PluginOption[] {
     /*原子化CSS插件*/
     unocssPlugin(),
     /* 打包分析 */
-    visualizer()
+    visualizer(),
+    // 自动按需引入组件
+    AutoRegistryComponents(),
+    // 自动按需引入依赖
+    AutoImportDeps(),
+    /* 监听配置文件改动重启 */
+    ConfigRestartPlugin()
   ]
 }
