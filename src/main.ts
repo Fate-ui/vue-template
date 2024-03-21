3ef70967f04e04239a5c68adc82e47a9b31cc64d
thunderchen <853524319@qq.com>
2024-03-21 10:38:17 +0800

    feat: add pinia cache

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
 import '@/style/index.scss'
 import { createPinia } from 'pinia'
+// https://prazdevs.github.io/pinia-plugin-persistedstate/guide/config.html
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from '@/router'
 import 'normalize.css'
 //原子化css
 import 'uno.css'
-
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 const app = createApp(App)
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 app.mount('#app')
