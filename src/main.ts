e057829d7ea70fbb06f31608ab4217f4daf75f24
ween <228574227@qq.com>
2024-04-25 13:58:34 +0800

    Revert "feat: add pinia cache"
    This reverts commit 3ef70967

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
 import { createApp } from 'vue'
 import '@/style/index.scss'
 import { createPinia } from 'pinia'
-// https://prazdevs.github.io/pinia-plugin-persistedstate/guide/config.html
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from '@/router'
 import 'normalize.css'
 //原子化css
 import 'uno.css'
-const pinia = createPinia()
-pinia.use(piniaPluginPersistedstate)
+
 const app = createApp(App)
-app.use(pinia)
+app.use(createPinia())
 app.use(router)
 app.mount('#app')
