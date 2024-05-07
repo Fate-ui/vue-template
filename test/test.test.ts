c31064cc8938f77863a0b5207eef29732836c005
ween <228574227@qq.com>
2024-05-07 13:34:46 +0800

    ✨ feat(全局): 添加stylelint

---
+++ test/test.test.ts
@@ -0,0 +1,22 @@
+/*
+ * @Author: thunderchen
+ * @Date: 2024-03-28 15:19:37
+ * @LastEditTime: 2024-03-28 15:25:48
+ * @email: 853524319@qq.com
+ * @Description: example
+ * ---------------------------------------------
+ * import {mount} from "@vue/test-utils"
+ * import {test,expect,describe} from "vitest"
+ * import testVue from ".vue"
+ *   describe('testVue', () => {
+ *    test('it should output text', () => {
+ *        const wrapper = mount(testVue, {
+ *            props: {
+ *                number: 0,
+ *            }
+ *          })
+ *        // 断言输出
+ *        expect(wrapper.html()).toContain('0')
+ *      })
+ *    })
+ */
--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      '#': path.resolve(__dirname, 'types'),
     },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
   },
